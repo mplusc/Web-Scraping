6e3acea45fe8d58429b341978fe002deca3f9180
mplusc555 <mplusc555@gmail.com>
2018-10-17 22:19:56 -0400

    Final

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars= mongo.db.mars
-    mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data, upsert=True)
+    mars = scrape_mars.scrape()
+    mars.update({}, mars, upsert=True)
     return redirect("/", code = 302)
 
 
