6e1bacf07aebccb5565d897337ad453ad23fef2e
mplusc555 <mplusc555@gmail.com>
2018-10-24 21:36:36 -0400

    Final

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.collection.find()
+    mars = mongo.db.collection.find_one()
 
     return render_template('index.html',mars=mars)
 
@@ -16,8 +17,8 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars= mongo.db.mars
-    mars = scrape_mars.scrape()
-    mars.update({}, mars, upsert=True)
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code = 302)
 
 
